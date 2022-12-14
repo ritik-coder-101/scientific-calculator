5e4eb203dc3caa69483893cc0ccd9787599e5e44
Ritik Roushan <116073992+ritik-coder-101@users.noreply.github.com>
2022-12-14 22:56:06 +0530

    Update cal.py

--- cal.py
+++ cal.py
@@ -29,7 +29,7 @@ def div(b,*a):
     div=b
     for x in a:
         div/=x
-    print(format(div),".12f")
+    print(format((div),".6f"))
 
 #modulous
 def mod(b,a):
@@ -57,7 +57,7 @@ def squareroot(b,*a):
             sum/=x
     else:
         sum=b
-    print(format(math.sqrt(sum),".12f"))
+    print("ANS= ",format(math.sqrt(sum),".6f"))
 
 #exponent
 def exponent(a,b):
@@ -66,8 +66,43 @@ def exponent(a,b):
 
 #trignometric function
 #sine function
-def sine(a):
+def sin(a):
     b=math.sin(a)
     print("value is: ",format((b),".4f"),)
 
-sine(89)
+#coscent function
+def cos(a):
+    b=math.cos(a)
+    print("Value is: ",format((b),".4f"))
+
+#tangent function
+def tan(a):
+    b=math.tan(a)
+    print("Value is: ",format((b),".4f"))
+
+#cosine function
+def cosec(a):
+    b=1/math.sin(a)
+    print("Value is: ",format((b),".4f"))
+
+#secant function
+def sec(a):
+    b=1/math.cos(a)
+    print("Value is: ",format((b),".4f"))
+
+#cotangent
+def cot(a):
+    b=1/math.tan(a)
+    print("Value is: ",format((b),".4f"))
+
+#degree to radian
+def radian(a):
+    b=math.radians(a)
+    print(format((b),".6f"))
+
+#radian to degree
+def degrees(a):
+    b=math.degrees(a)
+    print(format((b),".6f"))
+
+squareroot(67,89,74,73)
